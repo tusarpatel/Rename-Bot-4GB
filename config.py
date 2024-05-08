4816f47a399ab69398c556bdbfcfe15a5e6974f5
tusarpatel <167279662+tusarpatel@users.noreply.github.com>
2024-05-09 02:05:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7191204896:AAHX9lcUm0KXAJxZg6ut0NzenDpdi3t7r4o")
+API_ID = int(os.environ.get("API_ID", "24620300"))
+API_HASH = os.environ.get("API_HASH", "9a098f01aa56c836f2e34aee4b7ef963")
+OWNER = int(os.environ.get("OWNER", "6968763387"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "thumbnail_changer_pills_bot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "https://t.me/anime_in_hindi_pills")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002023182491"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "cluster0")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+STRING = os.environ.get("STRING", "BQF3rQwAHqbJg7GxDMQcrcKF-6xWRQEPF8yTGea9WQZM_hFIzh_AHk132YjlgZlla3gwPTq2iMT0JcPiw74LtE6t4sfybcmhs_cmj_UdQgiiXpfVroZhpysRfOjmtmQqVzCKgPysjo8Lt7jyM3-pRqLvOX-GnJ78NSy-zGcHVPJtTh6imy63y8rtcZtOQ7uG8vufwlogzJp-noOfsbVHUl658wTNpYWGj_YDWq0X4jDWog-Mu3imueuJiYnRsd3Zc3cGwr8BAx3vwQyphyrWryjFmWSHwzAjWrTxePTrfUzlmjM33MPRqB7BkqxTcgBaB2SVMX-_78uHz9eTxOi6pERefFyzIQAAAAGsoRQgAQ")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/75bfb550e3e7869e28e55.jpg")
 
 
 

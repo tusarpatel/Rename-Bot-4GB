6d8d50dcd4491b1c271fbdd446bd81d93fe4a0fb
tusarpatel <167279662+tusarpatel@users.noreply.github.com>
2024-05-09 01:35:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ API_HASH = os.environ.get("API_HASH", "")
 OWNER = int(os.environ.get("OWNER", ""))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "https://t.me/anime_in_hindi_pills")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002023182491"))
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")

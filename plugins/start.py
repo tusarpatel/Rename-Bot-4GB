72dbb4e762497ff4e9345d66dc7168520a2d2caf
tusarpatel <167279662+tusarpatel@users.noreply.github.com>
2024-05-12 21:54:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -39,11 +39,11 @@ async def start(client, message):
     await message.reply_photo(photo=BOT_PIC,
                                 caption=txt,
                                 reply_markup=InlineKeyboardMarkup(
-                                        [[InlineKeyboardButton("📢 Updates", url="https://t.me/Madflix_Bots"),
-                                        InlineKeyboardButton("💬 Support", url="https://t.me/MadflixBots_Support")],
+                                        [[InlineKeyboardButton("📢 Updates", url="https://t.me/about_tosuu"),
+                                        InlineKeyboardButton("💬 Support", url="https://t.me/about_tosuu")],
                                         [InlineKeyboardButton("🛠️ Help", callback_data='help'),
 				                        InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
-                                        [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://t.me/CallAdminRobot")]
+                                        [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://t.me/itz_tusarr")]
                                         ]))
     return
 
